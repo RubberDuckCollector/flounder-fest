ff677c787ea2678f66be2f9172ec341cc3378071
Luna Long <eggsim49@gmail.com>
2025-01-15 03:49:18 +0000

    input validation and failsave for a primary key related error

--- db-solution/input_validation.py
+++ db-solution/input_validation.py
@@ -40,7 +40,7 @@ def validate_episode(p_episode: int):
         pass
 
 
-def validate_final_episode_of_season(p_is_final_episode_of_season: int):
+def validate_final_episode_of_season(p_is_final_episode_of_season: int) -> bool:
     match p_is_final_episode_of_season:
         case 1:
             p_is_final_episode_of_season = True
@@ -50,9 +50,17 @@ def validate_final_episode_of_season(p_is_final_episode_of_season: int):
             print("Enter a valid number (1 or 2).")
             sys.exit(0)
 
+    return p_is_final_episode_of_season
+
+"""
+As you can see from here and below, the regexes are matched with single quotes.
+SQL requires them for dates and text.
+the `main-db.py` program adds quotes to the user inputs, so the user doesn't have to add the quotes.
+this would mean if they do add quotes, there would be 2 layers of quotes and it would cause an error.
+"""
 
 def validate_date(p_date: str):
-    date_regex = r"^\d{4}-\d{2}-\d{2}$"
+    date_regex = r"^'\d{4}-\d{2}-\d{2}'$"
     if re.match(date_regex, p_date):
         pass
     else:
@@ -60,13 +68,13 @@ def validate_date(p_date: str):
         sys.exit(0)
     try:
         # test that `date` is a valid date
-        datetime.strptime(p_date, "%Y-%m-%d")
+        datetime.strptime(p_date, "'%Y-%m-%d'")  # the ' chars make it work with SQL's syntax needing ' on dates and text
     except ValueError:
         print("Please enter a valid date.")
         sys.exit(0)
 
 
-def validate_tiebreak_episode(p_is_tiebreak_episode: int):
+def validate_tiebreak_episode(p_is_tiebreak_episode: int) -> bool:
     match p_is_tiebreak_episode:
         case 1:
             p_is_tiebreak_episode = True
@@ -76,9 +84,20 @@ def validate_tiebreak_episode(p_is_tiebreak_episode: int):
             print("Enter a valid number (1 or 2).")
             sys.exit(0)
 
+    return p_is_tiebreak_episode
+
+
+def validate_youtube_title(p_title: str):
+    title_pattern = r"^'(.*)'$"
+    if not re.match(title_pattern, p_title):
+        print("Please enter a valid youtube link.")
+        sys.exit(0)
+    else:
+        pass
 
 def validate_youtube_link(p_link: str):
-    link_pattern = r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=|playlist\?list=|channel/|c/)?[a-zA-Z0-9_-]+(&[a-zA-Z0-9_=-]*)*$"
+    # the ' chars make it work with SQL's syntax needing ' on dates and text
+    link_pattern = r"^'(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=|playlist\?list=|channel/|c/)?[a-zA-Z0-9_-]+(&[a-zA-Z0-9_=-]*)*'$"
     if not re.match(link_pattern, p_link):
         print("Please enter a valid youtube link.")
         sys.exit(0)
--- db-solution/main-db.py
+++ db-solution/main-db.py
@@ -51,6 +51,8 @@ def main():
                     conn.commit()
             except sqlite3.OperationalError as e:
                 print("Failed to create tables:", e)
+            print("Successfully initialised tables.")
+
 
             """
             Prompts user for data relating to the tracks played in an episode
@@ -58,8 +60,29 @@ def main():
             Using the `sqlite3` command and entering data manually is much slower.
             """
             if args.insert_tracks_played:
+                table = "tracks_played"
                 start_id = int(input("Enter start id (primary key): "))
                 input_validation.validate_start_id(start_id)
+                cursor.execute("SELECT MAX(id) FROM tracks_played")
+                max_id = cursor.fetchone()[0]  # fetchone() returns a tuple, so access the first element
+                if max_id is not None:
+                    if start_id <= max_id:
+                        message_list = [
+                            "You can't create records with the same primary keys. Also to be safe, you ",
+                            "can't create records with primary keys lower than what has already been ",
+                            "created (e.g if you make records 1-5, you're not allowed to make record 0 ",
+                            "through this automated process, since the program doesn't know how many ",
+                            "tracks - and therefore records - you will add).",
+                            f"\nThe current highest primary key in {table} is {max_id}, and the next viable primary key is {max_id+1}."
+                        ]
+
+                        for i in message_list:
+                            print(i)
+                        sys.exit(0)
+                    else:
+                        pass
+                else:
+                    pass
 
                 season = int(input("Enter season: "))
                 input_validation.validate_season(season)
@@ -68,43 +91,65 @@ def main():
                 input_validation.validate_episode(episode)
 
                 is_final_episode_of_season = int(input("Final episode of the season? (1 = yes, 2 = no): "))
-                input_validation.validate_final_episode_of_season(is_final_episode_of_season)
+                is_final_episode_of_season = input_validation.validate_final_episode_of_season(is_final_episode_of_season)
 
-                date = input("Enter date (YYYY-MM-DD): ")
+                date = input("Enter date (YYYY-MM-DD): ").strip()
+                date = f"'{date}'"
                 input_validation.validate_date(date)
 
                 is_tiebreak_episode = int(input("Is tiebreak episode? (1 = yes, 2 = no): "))
-                input_validation.validate_tiebreak_episode(is_tiebreak_episode)
+                is_tiebreak_episode = input_validation.validate_tiebreak_episode(is_tiebreak_episode)
 
-                twd98_video_title = input("Enter TWD98 video title: ")
+                twd98_video_title = input("Enter TWD98 video title (inside ''): ").strip()
+                twd98_video_title = f"'{twd98_video_title}'"
+                input_validation.validate_youtube_title(twd98_video_title)
 
-                twd98_video_link = input("Enter TWD98 video link: ")
+                twd98_video_link = input("Enter TWD98 video link (inside ''): ").strip()
+                twd98_video_link = f"'{twd98_video_link}'"
                 input_validation.validate_youtube_link(twd98_video_link)
 
-                # nmeade_video_title = input("Enter Nmeade video title: ")
 
-                # nmeade_video_link = input("Enter Nmeade video link: ")
-                # input_validation.validate_youtube_link(nmeade_video_link)
+                count = 0
+                tracks = []  # all the tracks they race in the episode
+                track = ""  # user input
+                while True:
+                    track = input(f"Enter track on race {count+1} : ")
+                    track = f"'{track}'"
+                    if track == "'-1'":
+                        break
+                    tracks.append(track)  # append the user input to the `tracks[]` list
+                    count += 1  # a count is kept for the race number on screen
+                num_races = len(tracks)
+                # pprint.pprint(tracks, indent=4)  # DEBUG
 
-                num_races = int(input("Enter number of races: "))
-                input_validation.validate_num_races(num_races)
-                
-                # ask for the tracks
-                tracks = []
-                for i in range(num_races):
-                    tracks.append(input(f"Enter track on race {i+1}: "))
-                pprint.pprint(tracks, indent=4)
+                if num_races < 1:  # presence check for tracks played in that episode
+                    print("Please enter at least 1 track.")
+                    sys.exit(0)
+                else:
+                    pass
+
+                print("Num races:", num_races)
 
                 # put together the SQL statements
                 statements = []
                 for i in range(num_races):
-                    statements.append(f"INSERT INTO tracks_played(id, season, episode, is_final_episode_of_season, date, is_tiebreak_episode, twd98_video_title, twd98_video_link, race, track) VALUES ({start_id}, {season}, {episode}, {is_final_episode_of_season}, {date}, {is_tiebreak_episode}, {twd98_video_title}, {twd98_video_link}, {i+1}, {tracks[i]})")
+                    #                                tracks_played
+                    statements.append(f"INSERT INTO {table}(id, season, episode, is_final_episode_of_season, date, is_tiebreak_episode, twd98_video_title, twd98_video_link, race, track) VALUES ({start_id}, {season}, {episode}, {is_final_episode_of_season}, {date}, {is_tiebreak_episode}, {twd98_video_title}, {twd98_video_link}, {i+1}, {tracks[i]})")
+                    # increment PRIMARY KEY
+                    # so the code doesn't try to assign the same primary key to multiple records,
+                    # which cannot make sense
+                    start_id += 1  
 
+                # pprint.pprint(statements, indent=4)  # DEBUG
 
+                # execute the statements generated
                 try:
-                    ...
+                    for i in statements:
+                        cursor.execute(i)
+                        conn.commit()
                 except sqlite3.OperationalError as e:
                     print("Error inserting data:", e)
+                print("Successfully inserted track data.")
 
             # user inputs
             # while True:
